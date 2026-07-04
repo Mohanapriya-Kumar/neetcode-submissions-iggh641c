497e20524b7ec6e0258931cf8622a2585ca9faf5
Mohanapriya Kumar <mohanapriya.kumar.129@gmail.com>
2026-07-05 00:32:53 +0530

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for(String str : strs){
+            sb.append(str.length()).append("#").append(str);
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> res = new ArrayList<>();
+        int j = 0;
+        int i = 0;
+        while(i < str.length()){
+            while(str.charAt(j) != '#') j++;
+            int len = Integer.parseInt(str.substring(i,j));
+            i = j + 1;
+            j = i + len;
+            res.add(str.substring(i,j));
+            i = j;
+        }
+        return res;
+    }
+}
