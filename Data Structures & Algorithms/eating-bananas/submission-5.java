dd91ef5884350fe8a4ae497cccecbaa674ff0f04
Mohanapriya Kumar <mohanapriya.kumar.129@gmail.com>
2026-07-13 23:02:46 +0530

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1, r = 0;
+        for(int pile : piles){
+            if(pile > r) r = pile;
+        }
+
+        while(l < r){
+            int m = l + (r - l)/2;
+            if(canEat(piles, h, m)) r = m;
+            else l = m + 1;
+        }
+        return l;
+    }
+    private boolean canEat(int[] piles, int h, int k){
+        int hrs = 0;
+        for(int pile : piles){
+            hrs += (int) Math.ceil((double)pile/k);
+        }
+        return hrs <= h;
+    }
+}
