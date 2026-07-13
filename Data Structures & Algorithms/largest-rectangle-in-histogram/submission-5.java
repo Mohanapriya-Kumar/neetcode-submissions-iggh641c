c187f1ffa0406eaf880c7d7dffa4a2e99b4ac9ea
Mohanapriya Kumar <mohanapriya.kumar.129@gmail.com>
2026-07-13 22:39:38 +0530

    Add: largest-rectangle-in-histogram - submission-5

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-5.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int largestRectangleArea(int[] h) {
+        //our goal is to find the right boundary which we will actually exclude
+        int n = h.length, maxArea = 0;
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i <= n; i++){
+            while(!st.isEmpty() && (i == n || h[i] < h[st.peek()])){
+                int ht = h[st.pop()];
+                int w = st.isEmpty() ? i : i - st.peek() - 1; 
+                maxArea = Math.max(maxArea, ht*w);
+            }
+            st.push(i);
+        }
+        return maxArea;
+    }
+}
