7358cb3903f07e34573fab650c6dcc235f122d9f
Mohanapriya Kumar <mohanapriya.kumar.129@gmail.com>
2026-07-14 23:42:18 +0530

    Add: palindrome-partitioning - submission-2

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        backtrack(0, s, new ArrayList<>(), res);
+        return res;
+    }
+    private void backtrack(int start, String s, List<String> curr, List<List<String>> res){
+        if(start == s.length()){
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        for(int end = start; end < s.length(); end++){
+            if(isPal(s, start, end)){
+                curr.add(s.substring(start, end+1));
+                backtrack(end + 1, s, curr, res);
+                curr.remove(curr.size() - 1);
+            }
+        }
+    }
+    private boolean isPal(String s, int l, int r){
+        while(l < r){
+            if(s.charAt(l) != s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
